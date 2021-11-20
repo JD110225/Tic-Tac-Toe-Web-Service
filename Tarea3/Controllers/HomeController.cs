bbdc69d6596f6aed2f6425b1415da02ac69073bb
Jose <gvsjosedavidd.6a@gmail.com>
2021-11-20 13:26:32 -0600

    Add referencia a mi titanic y métodos básicos del server en PHP

--- Tarea3/Controllers/HomeController.cs
+++ Tarea3/Controllers/HomeController.cs
@@ -9,19 +9,13 @@ namespace Tarea3.Controllers
 {
     public class HomeController : Controller
     {
+        public ServiceReference2.TikTakToePortClient client;
+        public HomeController()
+        {
+            client = new ServiceReference2.TikTakToePortClient();
+        }
         public ActionResult Index()
         {
-            servicioBraulio.ECCI_HolaMundoPortClient cliente = new servicioBraulio.ECCI_HolaMundoPortClient();
-            ViewBag.saludo = cliente.salude("Joseee");
-            miServicio.ECCI_HolaMundoPortClient cliente2= new miServicio.ECCI_HolaMundoPortClient();
-            string s2 = cliente2.salude("David");
-
-            //Con url de Braulio en wsdl tira error de que no llega la info
-            //Poniendo mi url: : 'Error al deserializar el cuerpo del mensaje de respuesta para la operación 'salude'. Fin de archivo no esperado. Los siguientes elementos no están cerrados: Envelope. Línea 2, posición 461.'
-            //USando localHost:System.ServiceModel.ProtocolException: 'El tipo de contenido text/html; charset=UTF-8 del mensaje de respuesta no coincide con el tipo de contenido del enlace (text/xml; charset=utf-8). 
-            string s = ViewBag.saludoLocal;
-            
-            Debug.WriteLine("Saludo de verdad: " + s2);
             return View();
         }
 
@@ -38,5 +32,14 @@ namespace Tarea3.Controllers
 
             return View();
         }
+
+        public string hello(string token, string id)
+        {
+            string tab=client.sendTableroClient();
+            Debug.WriteLine(tab);
+            return tab;
+
+        }
+
     }
 }
---
+++ Tarea3/Server/JuegoTikTakToe.php
@@ -0,0 +1,36 @@
+<?php 
+class JuegoTikTakToe{
+    public $tablero;
+  function __construct()
+  {
+    $this->tablero=array(
+      array('X','X','X'),
+      array('O','O','O'),
+      array('X','X','X')
+    );
+    $this->placeToken('H',1,1);
+  }
+
+function sendTableroClient(){
+    $resultado='';
+    foreach($this->tablero as $fila){
+      foreach($fila as $celda){
+        $resultado .= $celda;
+      }
+    }
+    return $resultado;
+  }
+
+function placeToken($token,$row,$col)
+{
+  $this->tablero[$row][$col]=$token;
+  return $this->sendTableroClient();
+}
+
+function getTablero(){
+    return $this->tablero;
+  }
+  
+
+}
+?>
