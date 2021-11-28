5bb05c69ad233ab2b6efb871d0f72dc3f5b6d98c
Edu F <efernandezbaldi@gmail.com>
2021-11-28 15:16:34 -0600

    condiciones gane

--- Tarea3/Server/JuegoTikTakToe.php
+++ Tarea3/Server/JuegoTikTakToe.php
@@ -31,6 +31,54 @@ function getTablero(){
     return $this->tablero;
   }
   
+function getFila($fila) {
+  return($this->tablero[$fila][0]+$this->tablero[$fila][1]+$this->tablero[$fila][2]);
+}
 
+function getColumna($columna) {
+  return($this->tablero[0][$columna]+$this->tablero[1][$columna]+$this->tablero[2][$columna]);
+}
+
+function getDiagonal1() {
+  return($this->tablero[0][0]+$this->tablero[1][1]+$this->tablero[2][2]+);
+}
+
+function getDiagonal2() {
+  return($this->tablero[2][2]+$this->tablero[1][1]+$this->tablero[0][0]+);
+}
+
+function checkWinningConditions() {
+  $winner = "";
+  $contador = 0;
+  while($contador<3 && $winner=="") {
+    if(getFila($contador)=="XXX") {
+      $winner = "X";
+    }
+    else if(getFila($contador)=="OOO") {
+      $winner = "O";
+    }
+    else if(getColumna($contador)=="XXX") {
+      $winner = "X";
+    }
+    else if(getColumna($contador)=="OOO") {
+      $winner = "O";
+    }
+  }
+  if($winner=="") {
+    if(getDiagonal1()=="XXX") {
+      $winner="X";
+    }
+    if(getDiagonal1()=="OOO") {
+      $winner="O";
+    }
+    if(getDiagonal2()=="XXX") {
+      $winner="X";
+    }
+    if(getDiagonal2()=="OOO") {
+      $winner="O";
+    }
+  }
+  return($winner);
+}
 }
 ?>
